308232fd95000081ecdd05a4d06d045052675bc0
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2020-11-13 22:57:20 +0600

    Update and rename GCD-LCM CODE to GCD & LCM.cpp

--- GCD & LCM.cpp
+++ GCD & LCM.cpp
@@ -1,16 +1,18 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-#define filein freopen("in.txt","r",stdin);
+
 int gcd(int a, int b) {
 //    if ( b == 0 ) return a;
 //    else return gcd(b, a%b);
 
     return b==0? a:gcd(b,a%b);
 }
+
 int lcm( int a, int b) {
     return ( a/__gcd(a,b))*b;
 }
+
 int main() {
     //filein;
     int a, b;
