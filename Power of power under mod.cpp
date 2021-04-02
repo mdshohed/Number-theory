18d41cf8ab9528ed9261ec23f573da740dccac90
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2021-04-03 00:11:07 +0600

    Create Power of power under mod.cpp

---
+++ Power of power under mod.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mod = 1e9+7;
+
+ll binpow(ll a, ll b, int mod) {
+    int sum = 1;
+    a%=mod;
+    while(b){
+        if (b&1) sum = (sum*a)%mod;
+        a = (a*a)%mod;
+        b/=2;
+    }
+    return sum;
+}
+
+int main() {
+    //filein( "in.txt");
+    //ios_base::sync_with_stdio(false),cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while ( t-- ) {
+        ll a, b,c;
+        cin >> a >> b >> c;
+        cout << binpow( a, binpow(b, c, mod-1), mod) << endl;
+    }
+}
+
+/*        
+            c
+          b^  
+    x = A^
+    
+    
+*/
