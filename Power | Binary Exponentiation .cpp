d0c5d7a6e25d03b4566fe8f2becebd9732218256
Shohedul Islam <mdshohed170@gmail.com>
2021-10-18 21:06:20 +0600

    Update Power | Binary Exponentiation .cpp

--- Power | Binary Exponentiation .cpp
+++ Power | Binary Exponentiation .cpp
@@ -5,21 +5,28 @@ algorithm: binary Exponentiation
 #include <bits/stdc++.h>
 using namespace std;
 
+const int mod = 1e9+7; 
 int binpow(int a, int b) {
-    int ans = 1;
-    while ( b>0) {
-        if ( b&1) ans *=a;
-        a*=a;
-        b>>=1;
+    a %=mod; 
+    int sum = 1;
+    while (b) {
+        if (b&1) sum = (sum * 1LL * a) % mod; 
+        a = (a * 1LL * a) % mod;
+        b >>= 1; 
     }
-    return ans;
+    return sum; 
 }
 
-int main(){
-    int a, b;
-    cin >> a >> b;
-
-    int x = binpow(a,b);
-    cout << x << endl;
-    return 0;
+int main() {
+#ifdef Local
+    ios_base::sync_with_stdio(false),cin.tie(nullptr); 
+#else
+    freopen( "in.txt", "r", stdin);
+    //freopen( "out.txt","w", stdout);
+#endif
+    int a, b; 
+    cin >> a >> b; 
+    cout << binpow( a, b) << endl; 
+    return 0; 
 }
+  
