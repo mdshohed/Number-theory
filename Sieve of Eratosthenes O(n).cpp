8361cceb426e85c3e0cee54fe129685a9806aa50
Shohedul Islam <mdshohed170@gmail.com>
2023-03-01 14:51:50 +0600

    Update Sieve of Eratosthenes O(n).cpp

--- Sieve of Eratosthenes O(n).cpp
+++ Sieve of Eratosthenes O(n).cpp
@@ -11,3 +11,20 @@ void seive(){
 	for(int i:prime) cout << i << " ";
 	cout << endl;
 }
+
+const int mxN = 1e7+7;
+vector<ll> p;
+bool mark[mxN];
+void sieve(){
+    p.push_back(2);
+    for(ll i = 3; i<=mxN; i+=2){ 
+        if(!mark[i]) { 
+            p.push_back(i); 
+            for(ll j = i*i; j<=mxN; j+=i+i ) {
+               mark[j] = 1; 
+            }
+        }
+    }
+    for(int i: p) cout << i << " "; 
+    cout << endl;
+}
